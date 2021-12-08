3b429afacec272a588e7a4c5c72fe90f5abcc57e
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-08 18:14:10 -0300

    feat: adiciona migration dos usuarios

---
+++ database/migrations/2021_12_08_195520_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('nome', 60);
+            $table->string('cpf', 11);
+            $table->date('dataNascimento');
+            $table->string('email', 255);
+            $table->string('telefone', 10);
+            $table->string('logradouro', 100);
+            $table->string('cidade', 100);
+            $table->string('estado', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
