8a7f42a8d01d3713745edf9f2f4ffc8bfafbbbcd
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-09 11:54:57 -0300

    docs: adiciona documentação da interface UserRepository

--- app/Contracts/Repositories/UserRepository.php
+++ app/Contracts/Repositories/UserRepository.php
@@ -5,13 +5,42 @@ use App\Entities\User;
 
 interface UserRepository
 {
+    /**
+     * Retorna todos usuários cadastrados no banco de dados
+     *
+     * @return array
+     */
     public function getAll();
 
+    /**
+     * Retorna usuário pelo id do banco de dados
+     *
+     * @param int $id
+     * @return \App\Entities\User|null
+     */
     public function getById(int $id);
 
+    /**
+     * Salva usuário no banco de dados
+     *
+     * @param \App\Entities\User $user
+     * @return \App\Entities\User
+     */
     public function save(User $user);
 
+    /**
+     * Atualiza usuário no banco de dados
+     *
+     * @param \App\Entities\User $user
+     * @return \App\Entities\User
+     */
     public function update(User $user);
 
+    /**
+     * Deleta usuário no banco de dados
+     *
+     * @param \App\Entities\User $user
+     * @return bool
+     */
     public function delete(User $user);
 }
