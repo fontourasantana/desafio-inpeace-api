098f911642fb73f2fc25d1c8397be8d1d2188be0
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-09 12:14:08 -0300

    docs: adiciona documentação da interface UserFactory

--- app/Contracts/Factories/UserFactory.php
+++ app/Contracts/Factories/UserFactory.php
@@ -6,7 +6,19 @@ use App\Entities\User;
 
 interface UserFactory
 {
+    /**
+     * Cria entidade usuário pela request da api
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \App\Entities\User
+     */
     public function makeFromRequest(Request $request);
 
+    /**
+     * Cria entidade usuário por lista de atributos
+     *
+     * @param array $attributes
+     * @return \App\Entities\User
+     */
     public function makeFromAttributes(array $attributes);
 }
