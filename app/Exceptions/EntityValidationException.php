33e813a8a00fdec57b84fd695428e30e6cc2e4d5
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-09 13:05:54 -0300

    docs: adiciona documentação da exceção EntityValidationException

--- app/Exceptions/EntityValidationException.php
+++ app/Exceptions/EntityValidationException.php
@@ -3,14 +3,28 @@ namespace App\Exceptions;
 
 class EntityValidationException extends \Exception
 {
+    /**
+     * @var array
+     */
     private $errors;
 
+    /**
+     * Cria exceção de validação
+     *
+     * @param array $errors
+     * @return void
+     */
     public function __construct(array $errors)
     {
         parent::__construct($errors[0]);
         $this->errors = $errors;
     }
 
+    /**
+     * Retorna erros que ocorreram durante a validação
+     *
+     * @return array
+     */
     public function getErrors()
     {
         return $this->errors;
