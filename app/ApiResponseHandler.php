ca09e5133414b2c5878ba3a9efc7ec3a9f0b00e2
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-09 13:00:14 -0300

    refactor: atualizados métodos da classe ApiResponseHandler
    Também foi adicionado a documentação relacionada a classe

--- app/ApiResponseHandler.php
+++ app/ApiResponseHandler.php
@@ -3,67 +3,121 @@ namespace App;
 
 class ApiResponseHandler
 {
+    /**
+     * @var array
+     */
     private $attributes = [];
 
+    /**
+     * @var array
+     */
     private $keys = ['status', 'message', 'data'];
 
-    public function success($message = '', $code = 200)
+    /**
+     * Define resposta da api como sucesso, message e código
+     *
+     * @param string $message
+     * @param int $code
+     * @return $this
+     */
+    public function success(string $message = '', int $code = 200)
     {
-        // verificar se é uma string
         if (!empty($message)) {
             $this->attributes['message'] = $message;
         }
+
         $this->attributes['status'] = 'success';
         $this->attributes['code'] = $code;
         return $this;
     }
 
-    public function created($message = '', $code = 201)
+    /**
+     * Define resposta da api como error, message e código
+     *
+     * @param string $message
+     * @param int $code
+     * @return $this
+     */
+    public function error(string $message = '', int $code = 400)
     {
         if (!empty($message)) {
             $this->attributes['message'] = $message;
         }
-        $this->attributes['status'] = 'success';
-        $this->attributes['code'] = $code;
-        return $this;
-    }
 
-    public function error($message = '', $code = 400)
-    {
-        if (!empty($message)) {
-            $this->attributes['message'] = $message;
-        }
         $this->attributes['status'] = 'error';
         $this->attributes['code'] = $code;
         return $this;
     }
 
-    public function data($data)
+    /**
+     * Define resposta da api como sucesso, message e código 201
+     *
+     * @param string $message
+     * @return $this
+     */
+    public function created(string $message = '')
+    {
+        return $this->success($message, 201);
+    }
+
+    /**
+     * Define dados da resposta da api
+     *
+     * @param array $data
+     * @return $this
+     */
+    public function data(array $data)
     {
         $this->attributes['data'] = $data;
         return $this;
     }
 
+    /**
+     * Cria resposta retornada pela api
+     *
+     * @return \Illuminate\Http\Response
+     *
+     * @throws \InvalidArgumentException
+     */
     public function send()
     {
-        $response = [];
-        $keys = $this->keys;
-        $attributes = $this->attributes;
-
-        if (!isset($attributes['status'])) {
-            throw new \Exception(
+        if (!$this->validate()) {
+            throw new \InvalidArgumentException(
                 'É necessário informar pelo menos um modo de resposta'
             );
         }
 
-        foreach ($keys as $key) {
-            if (isset($attributes[$key])) {
-                $response = array_merge($response, [
-                    "$key" => $attributes[$key],
-                ]);
+        $response = $this->prepare();
+
+        return response()->json($response['data'], $response['code']);
+    }
+
+    /**
+     * Valida dados necessários para criar resposta
+     *
+     * @return bool
+     */
+    private function validate()
+    {
+        return isset($this->attributes['status']);
+    }
+
+    /**
+     * Prepara resposta com os dados informados
+     *
+     * @return array
+     */
+    private function prepare()
+    {
+        $data = [];
+        $code = $this->attributes['code'];
+
+        foreach ($this->keys as $key) {
+            if (isset($this->attributes[$key])) {
+                $data[$key] = $this->attributes[$key];
             }
         }
 
-        return response()->json($response, $attributes['code']);
+        return compact('data', 'code');
     }
 }
