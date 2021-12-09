a50157f7c8e577135faf743049a09e6e6c1d2e9b
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-09 12:07:15 -0300

    docs: adiciona documentação da interface UserService

--- app/Contracts/Services/UserService.php
+++ app/Contracts/Services/UserService.php
@@ -5,13 +5,50 @@ use App\Entities\User;
 
 interface UserService
 {
+    /**
+     * Retorna todos usuários cadastrados no repositório
+     *
+     * @return array
+     */
     public function getAll();
 
+    /**
+     * Retorna usuário pelo id no repositório
+     *
+     * @param int $id
+     * @return \App\Entities\User|null
+     *
+     * @throws \App\Exceptions\EntityNotFoundException
+     */
     public function getById(int $id);
 
+    /**
+     * Salva usuário no repositório
+     *
+     * @param \App\Entities\User $user
+     * @return \App\Entities\User
+     *
+     * @throws \App\Exceptions\EntityValidationException
+     */
     public function save(User $user);
 
+    /**
+     * Atualiza usuário no repositório
+     *
+     * @param \App\Entities\User $user
+     * @return \App\Entities\User
+     *
+     * @throws \App\Exceptions\EntityValidationException
+     */
     public function update(User $user);
 
+    /**
+     * Deleta usuário no repositório
+     *
+     * @param \App\Entities\User $user
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
     public function delete(User $user);
 }
